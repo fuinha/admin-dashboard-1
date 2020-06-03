cd0abf162714b314f3ab73dbba9a320a26d55b6b
Kevin Siegler <kevinsiegler54@gmail.com>
2020-06-03 16:13:04 -0500

    pre-eject

---
+++ src/localservice/serve.js
@@ -0,0 +1,45 @@
+// @flow
+const fs = require("fs");
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const middlewares = jsonServer.defaults();
+
+// TODO: process path input parameter and ensure it's a valid cred repo
+const validPath = "../sc-cred/initiatives/";
+const loadFiles = () => {
+  const db = {};
+  db.initiatives = loadInitiatives();
+  fs.writeFileSync("db.json", JSON.stringify(db), "utf8");
+};
+
+const loadInitiatives = () => {
+  const initiatives = [];
+  const files = fs
+    .readdirSync(validPath)
+    .filter((f) => RegExp(".json$").test(f));
+  for (const fileName of files) {
+    const initObj = JSON.parse(fs.readFileSync(validPath + fileName, "utf8"));
+    const shapedInit = {
+      ...initObj,
+      id: initiatives.length,
+    };
+    initiatives.push(shapedInit);
+  }
+  return initiatives;
+};
+
+// TODO load contributors on the client side
+// it's not really useful to do any of this processing here
+// const loadContributors = () => {
+//   contributors = [];
+// };
+
+if (process.mainModule.filename === __filename) {
+  loadFiles();
+  const router = jsonServer.router("db.json");
+  server.use(middlewares);
+  server.use(router);
+  server.listen(3000, () => {
+    console.log("JSON Server is running");
+  });
+} else module.exports = loadFiles;
